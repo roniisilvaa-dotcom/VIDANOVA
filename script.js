9e5e7d8c187b408631aea6a84f3920b09e1f7836
Roni Silva <roniisilvaa@icloud.com>
2026-04-21 13:19:25 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-function openPlanner(){
-  document.querySelectorAll('.page-view').forEach(p=>p.classList.remove('is-active'));
-  document.getElementById('planner-view').classList.add('is-active');
-}
+Unsupported Media Type
