aa7db1c49417898fdcaac8564e9cf7ed08309764
Roni Silva <ronysiilvaa1@gmail.com>
2026-05-25 22:42:18 -0400

    fix: restore stable layout and force PWA cache refresh

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "vida-nova-v23";
-const CACHE_DYNAMIC = "vida-nova-dynamic-v22";
+const CACHE_NAME = "vida-nova-v24";
+const CACHE_DYNAMIC = "vida-nova-dynamic-v23";
 const APP_SHELL = [
   "./",
   "./index.html",
